7d78824bd8fd02b19256c3f259cdc0a16ff48b6a
bhuwanb23 <bhuwanseervi4567@gmail.com>
2026-07-13 15:55:30 +0530

    chore: allow ESM parsing for landing config in eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,9 @@
 module.exports = {
   root: true,
+  overrides: [
+    {
+      files: ['*.config.js', 'landing/**/*.js', 'landing/**/*.jsx'],
+      parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+    },
+  ],
 };
