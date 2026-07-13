3729d7e90afad80e1d22fe93669920a0591aab3a
bhuwanb23 <bhuwanseervi4567@gmail.com>
2026-07-13 15:55:18 +0530

    test: add @sentry/react-native jest mock

---
+++ urbanflow_app/__mocks__/sentryMock.js
@@ -0,0 +1,18 @@
+const init = () => {};
+const captureException = () => {};
+const captureMessage = () => {};
+const addBreadcrumb = () => {};
+const setUser = () => {};
+const setTag = () => {};
+const navigate = () => {};
+
+export default {
+  init,
+  captureException,
+  captureMessage,
+  addBreadcrumb,
+  setUser,
+  setTag,
+  navigate,
+};
+export { init, captureException, captureMessage, addBreadcrumb, setUser, setTag, navigate };
